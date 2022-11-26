c8207899eeeca6f32e1f090cfa01719fc5627a51
Ed Allonby <edward.allonby@gmail.com>
2022-11-26 18:52:09 +0000

    Use tailwind and image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
-      <Link href="/blog/123?id=abc">Go to a blog</Link>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Link className="hover:underline" href="/blog/123?id=abc">
+        Go to a blog
+      </Link>
+      <Image src="/placeholder.jpg" alt="forest" width={400} height={400} />
     </>
   );
 }
